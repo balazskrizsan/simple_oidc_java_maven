43558838c50668c92d9569b3d5387542ecc40fbe
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-07-20 18:51:16 +0200

    IOidcService updates

--- src/main/java/com/kbalazsworks/simple_oidc/services/IOidcService.java
+++ src/main/java/com/kbalazsworks/simple_oidc/services/IOidcService.java
@@ -3,9 +3,30 @@ package com.kbalazsworks.simple_oidc.services;
 import com.kbalazsworks.simple_oidc.entities.AccessTokenRawResponse;
 import com.kbalazsworks.simple_oidc.entities.BasicAuth;
 import com.kbalazsworks.simple_oidc.entities.IntrospectRawResponse;
+import com.kbalazsworks.simple_oidc.entities.JwksKeys;
+import com.kbalazsworks.simple_oidc.exceptions.OidcException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcExpiredTokenException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcJwksVerificationException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcJwtParseException;
+
+import java.util.List;
 
 public interface IOidcService
 {
     AccessTokenRawResponse callTokenEndpoint(String clientId, String clientSecret, String scope, String grantType);
+
     IntrospectRawResponse callIntrospectEndpoint(String accessToken, BasicAuth basicAuth);
+
+    JwksKeys callJwksEndpoint();
+
+    void checkScopesInToken(String token, List<String> scopes) throws OidcException;
+
+    void checkValidated(String token)
+    throws OidcExpiredTokenException, OidcJwksVerificationException, OidcJwtParseException;
+
+    void checkExpiredToken(String token) throws OidcExpiredTokenException, OidcJwtParseException;
+
+    Boolean isJwksVerifiedToken(String token);
+
+    void checkJwksVerifiedToken(String token) throws OidcJwksVerificationException;
 }
