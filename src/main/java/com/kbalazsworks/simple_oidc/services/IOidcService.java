aa9eaa09cbfb9f1e97084a522e9fd11c23895e27
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-07-26 23:41:34 +0200

    IOidcService update

--- src/main/java/com/kbalazsworks/simple_oidc/services/IOidcService.java
+++ src/main/java/com/kbalazsworks/simple_oidc/services/IOidcService.java
@@ -3,12 +3,43 @@ package com.kbalazsworks.simple_oidc.services;
 import com.kbalazsworks.simple_oidc.entities.AccessTokenRawResponse;
 import com.kbalazsworks.simple_oidc.entities.BasicAuth;
 import com.kbalazsworks.simple_oidc.entities.IntrospectRawResponse;
+import com.kbalazsworks.simple_oidc.entities.JwksKeys;
 import com.kbalazsworks.simple_oidc.exceptions.OidcApiException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcExpiredTokenException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcJwksVerificationException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcJwtParseException;
+import com.kbalazsworks.simple_oidc.exceptions.OidcScopeException;
+import lombok.NonNull;
+
+import java.util.List;
 
 public interface IOidcService
 {
-    AccessTokenRawResponse callTokenEndpoint(String clientId, String clientSecret, String scope, String grantType)
-    throws OidcApiException;
+    @NonNull AccessTokenRawResponse callTokenEndpoint(
+        @NonNull String clientId,
+        @NonNull String clientSecret,
+        @NonNull String scope,
+        @NonNull String grantType
+    ) throws OidcApiException;
 
-    IntrospectRawResponse callIntrospectEndpoint(String accessToken, BasicAuth basicAuth) throws OidcApiException;
+    @NonNull IntrospectRawResponse callIntrospectEndpoint(
+        @NonNull String accessToken,
+        @NonNull BasicAuth basicAuth
+    ) throws OidcApiException;
+
+    @NonNull JwksKeys callJwksEndpoint() throws OidcApiException;
+
+    void checkScopesInToken(@NonNull String token, @NonNull List<String> scopes)
+    throws OidcScopeException, OidcJwtParseException, OidcExpiredTokenException, OidcJwksVerificationException;
+
+    void checkValidated(String token)
+    throws OidcExpiredTokenException, OidcJwksVerificationException, OidcJwtParseException;
+
+    void checkExpiredToken(@NonNull String token) throws OidcExpiredTokenException, OidcJwtParseException;
+
+    @NonNull Boolean isExpiredToken(@NonNull String token) throws OidcJwtParseException;
+
+    void checkJwksVerifiedToken(@NonNull String token) throws OidcJwksVerificationException;
+
+    @NonNull Boolean isJwksVerifiedToken(@NonNull String token);
 }
