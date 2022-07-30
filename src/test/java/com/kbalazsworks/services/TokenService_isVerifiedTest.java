c1c4dd93b8d8dccb5b7614af2164f33872afbbd3
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-07-30 13:18:19 +0200

    TokenService_isVerifiedTest added

---
+++ src/test/java/com/kbalazsworks/services/TokenService_isVerifiedTest.java
@@ -0,0 +1,28 @@
+package com.kbalazsworks.services;
+
+import com.kbalazsworks.test_helpers.AbstractTest;
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import java.security.PublicKey;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TokenService_isVerifiedTest extends AbstractTest
+{
+    @Test
+    @SneakyThrows
+    public void validKeyData_returnTrue()
+    {
+        // Arrange
+        PublicKey testedPublicKey  = getValidPrivateKey();
+        byte[]    testedSignedData = getValidSignedData();
+        byte[]    testedSignature  = getValidSignature();
+
+        // Act
+        Boolean actual = getTokenService().isVerified(testedPublicKey, testedSignedData, testedSignature);
+
+        // Assert
+        assertThat(actual).isTrue();
+    }
+}
