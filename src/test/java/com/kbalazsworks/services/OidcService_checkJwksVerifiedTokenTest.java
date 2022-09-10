27adfe9542b175ae167b66b6ee9b706904e089c6
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-09-10 20:55:25 +0200

    OidcService_checkExpiredTokenTest and OidcService_checkJwksVerifiedTokenTest added

---
+++ src/test/java/com/kbalazsworks/services/OidcService_checkJwksVerifiedTokenTest.java
@@ -0,0 +1,25 @@
+package com.kbalazsworks.services;
+
+import com.kbalazsworks.simple_oidc.entities.AccessTokenRawResponse;
+import com.kbalazsworks.test_helpers.AbstractTest;
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OidcService_checkJwksVerifiedTokenTest extends AbstractTest
+{
+    @Test
+    @SneakyThrows
+    public void freshNewToken_doesNothing()
+    {
+        // Arrange
+        AccessTokenRawResponse testedToken = requestTokenFromIds();
+
+        // Act
+        getOidcService().checkJwksVerifiedToken(testedToken.getAccessToken());
+
+        // Assert
+        assertTrue(true);
+    }
+}
